3267c8eab25e7dbe104c24a2d3b0c033ae4ac6d4
Jose Arrisola Jr <37916204+RetroDawg@users.noreply.github.com>
2020-05-12 19:17:50 -0500

    Update crowdstrikeInstaller.py

--- crowdstrikeInstaller.py
+++ crowdstrikeInstaller.py
@@ -127,7 +127,7 @@ def runUbuntu18_04():
     url = 'https://repo.geos.tamu.edu/common-configs/toolbox/source/sensor-download/Ubuntu14-16-18/falcon-sensor_5.31.0-9606_amd64.deb'
     urllib.request.urlretrieve(url, '/tmp/falcon-sensor_5.31.0-9606_amd64.deb')
     try:
-        stream = os.popen('apt-get -f install -y /tmp/falcon-sensor_5.31.0-9606_amd64.deb')
+        stream = os.popen('apt-get -f -y install /tmp/falcon-sensor_5.31.0-9606_amd64.deb')
         output = stream.read()
 
         stream = os.popen('/opt/CrowdStrike/falconctl -s --cid=941077C3CE5C44C4BDF4EB3D3C1CE22F-AE')
@@ -150,7 +150,7 @@ def runUbuntu20_04():
     print("Detected Ubuntu 20.04 running runUbuntu20_04()")
     url = 'https://repo.geos.tamu.edu/common-configs/toolbox/source/sensor-download/Ubuntu14-16-18/falcon-sensor_5.31.0-9606_amd64.deb'
     try:
-        stream = os.popen('apt-get -f install -y /tmp/falcon-sensor_5.31.0-9606_amd64.deb')
+        stream = os.popen('apt-get -f -y install /tmp/falcon-sensor_5.31.0-9606_amd64.deb')
         output = stream.read()
 
         stream = os.popen('/opt/CrowdStrike/falconctl -s --cid=941077C3CE5C44C4BDF4EB3D3C1CE22F-AE')
